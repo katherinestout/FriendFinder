02db4d2038d1dbd5fa7e043fbd33adf50982f202
katie stout <katiestout@katies-MBP.home>
2018-06-11 21:18:02 -0400

    this

--- app/data/friends.js
+++ app/data/friends.js
@@ -2,17 +2,17 @@
 //the array of friend options
 
 var friends = [{name: "Bob Ross",
-            image:"http://reductress.com/wp-content/uploads/2018/01/Karen-Bonaker-mystic-mountain-with-Bob-1200x627-820x500.jpg" ;
+            image:"http://reductress.com/wp-content/uploads/2018/01/Karen-Bonaker-mystic-mountain-with-Bob-1200x627-820x500.jpg",
             scores: [4,1,2,3,6,3,5,3,1,1],
             total: 29},
 
             {name: "Brittany Spears",
-            image:"https://pbs.twimg.com/profile_images/955803874463571969/-R8etznz_400x400.jpg" ;
+            image:"https://pbs.twimg.com/profile_images/955803874463571969/-R8etznz_400x400.jpg",
             scores: [5,4,3,2,1,6,3,4,2,4],
             total: 34},
 
             {name: "Carrie Underwood",
-            image: "https://img.discogs.com/SGBi7FBEWeWjcCeGBYxswoEaiMc=/599x798/smart/filters:strip_icc():format(jpeg):mode_rgb():quality(90)/discogs-images/A-1011680-1340714590-7935.jpeg.jpg";
+            image: "https://img.discogs.com/SGBi7FBEWeWjcCeGBYxswoEaiMc=/599x798/smart/filters:strip_icc():format(jpeg):mode_rgb():quality(90)/discogs-images/A-1011680-1340714590-7935.jpeg.jpg",
             scores:[5,1,1,1,2,3,1,2,3,2],
             total: 21},
             ];
